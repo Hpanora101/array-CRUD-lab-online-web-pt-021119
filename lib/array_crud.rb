8835d458a31cae4eab28138a1a951c4812156e63
Henry Panora <hpanora@gmail.com>
2019-02-23 02:01:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,20 +22,27 @@ end
 def remove_element_from_end_of_array(array)
 
 array= ["wow", "I", "am", "really"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
+array= ["wow", "I", "am", "really"]
+array.shift
 
 end
 
 def retrieve_element_from_index(array, index_number)
+array= ["wow", "I", "am", "really"]
+array[2]
 
 end
 
 def retrieve_first_element_from_array(array)
+array= ["wow", "I", "am", "really"]
+array[0]
 
 end
 
 def retrieve_last_element_from_array(array)
-
+array[-1]
 end
